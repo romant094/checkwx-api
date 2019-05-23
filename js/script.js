1108ab838f33cfbe9cca5ac00ff7bc382c8212f4
Anton Romankov <romant094@gmail.com>
2019-05-23 18:10:48 +0300

    Added UX on error

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const d = document,
-    fetchBtn = d.querySelector('#fetch-btn');
+    fetchBtn = d.querySelector('#fetch-btn'),
+    infoBlock = d.querySelector('.information'),
+    errorBlock = d.querySelector('.error');
 
 const _url = 'https://api.checkwx.com';
 const _apiKey = 'ef56bcec85ea6dc0afbe008abc';
@@ -11,7 +13,7 @@ function makeQuery() {
     const query = ['metar', 'taf'];
 
     query.forEach((q) => {
-        fetch(`${_url}/${q}/${code}`, {
+        fetch(`${_url}/${q}/${code}/decoded`, {
             headers: {
                 'x-api-key': _apiKey
             }
@@ -19,10 +21,33 @@ function makeQuery() {
             .then(res => res.json())
             .then((res) => {
                 const data = res.data[0];
-                d.querySelector('.' + q).textContent = data;
+
+                console.log(data);
+
+                if (data) {
+                    showInfo(true);
+                    const weather = d.querySelector('.' + q + ' .info');
+                    const airport = infoBlock.querySelector('.airport .info');
+
+                    weather.textContent = data.raw_text;
+
+                    airport.textContent = `[${data.icao.toUpperCase()}] ${data.station.name}`;
+                } else {
+                    showInfo(false);
+                }
             })
             .catch(e => {
                 console.log(e);
             });
     });
+}
+
+function showInfo(param) {
+    if (param === true) {
+        infoBlock.classList.remove('disabled');
+        errorBlock.classList.add('disabled');
+    } else {
+        infoBlock.classList.add('disabled');
+        errorBlock.classList.remove('disabled');
+    }
 }
