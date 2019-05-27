97d6d4f97cfc5faeb5a32ab9afcc6f1742144dd0
Anton Romankov <romant094@gmail.com>
2019-05-27 17:42:50 +0300

    Added new output algorithm.

--- js/script.js
+++ js/script.js
@@ -6,14 +6,100 @@ const d = document,
     metarDecBlock = d.querySelector('.metar-decryption');
 
 const _apiUrls = {
-    yandex: 'https://translate.yandex.net/api/v1.5/tr.json/translate',
+    yandex: 'https://translateYandex.yandex.net/api/v1.5/tr.json/translateYandex',
     checkwx: 'https://api.checkwx.com'
 };
 const _apiKeys = {
     yandex: 'trnsl.1.1.20190524T141447Z.0213b37ed61910ce.e7ba9c225b0757bc761e59234afb437f37c8fefb',
     checkwx: 'ef56bcec85ea6dc0afbe008abc'
 };
-const _runwayInfo = {};
+
+const _dictionary = {
+    clouds: {
+        SCT: 'разбросанная',
+        SKS: 'ясно',
+        NSC: 'не существенная',
+        FEW: 'незначит., рассеянная',
+        SKT: 'отдельная разбросанная',
+        BKN: 'значительная разбросанная',
+        OVC: 'сплошная',
+        CB: 'кучево-дождевая',
+        CAVOK: 'условия хорошие'
+    }
+};
+
+const _propSequense = [
+    {
+        metarProp: 'wind',
+        neededProps: [
+            {
+                name: 'speed_mps',
+                prefix: 'Ветер',
+                postfix: ' м/с'
+            },
+            {
+                name: 'degrees',
+                prefix: '',
+                postfix: '°'
+            }
+        ]
+    },
+    {
+        metarProp: 'visibility',
+        neededProps: [
+            {
+                name: 'meters_float',
+                prefix: 'Видимость',
+                postfix: ' км'
+            }
+        ]
+    },
+    {
+        metarProp: 'clouds',
+        neededProps: [
+            {
+                name: 'base_feet_agl',
+                prefix: '',
+                postfix: "'"
+            },
+            {
+                name: 'code',
+                prefix: '',
+                postfix: ''
+            }
+        ]
+    },
+    {
+        metarProp: 'temperature',
+        neededProps: [
+            {
+                name: 'celsius',
+                prefix: 'Температура воздуха',
+                postfix: '°C'
+            }
+        ]
+    },
+    {
+        metarProp: 'dewpoint',
+        neededProps: [
+            {
+                name: 'celsius',
+                prefix: 'Точка росы',
+                postfix: ''
+            }
+        ]
+    },
+    {
+        metarProp: 'barometer',
+        neededProps: [
+            {
+                name: 'mb',
+                prefix: 'Давление на уровне моря',
+                postfix: ' ГПа'
+            }
+        ]
+    }
+];
 
 fetchBtn.addEventListener('click', makeQuery);
 codeInput.addEventListener('keyup', function (e) {
@@ -23,6 +109,7 @@ codeInput.addEventListener('keyup', function (e) {
 });
 
 function makeQuery() {
+    console.clear();
     const query = ['metar', 'taf'];
 
     query.forEach((q) => {
@@ -33,6 +120,7 @@ function makeQuery() {
         })
             .then(res => res.json())
             .then(res => {
+                const outputBlock = metarDecBlock.querySelector(' .info');
                 const data = res.data[0];
 
                 if (q === 'metar') console.log(data);
@@ -47,11 +135,11 @@ function makeQuery() {
                     const {name} = data.station;
                     const {icao} = data;
                     airport.textContent = `[${icao.toUpperCase()}] ${name}`;
+
+                    if (q === 'metar') outputBlock.textContent = metarDecr(data);
                 } else {
                     showInfo(false);
                 }
-
-                metarDecr(data);
             })
             .catch(e => {
                 console.log(e);
@@ -65,66 +153,57 @@ function showInfo(param) {
         infoBlock.classList.remove('disabled');
         errorBlock.classList.add('disabled');
         metarDecBlock.classList.remove('disabled');
+        metarDecBlock.classList.remove('disabled');
     } else {
         infoBlock.classList.add('disabled');
         errorBlock.classList.remove('disabled');
+        metarDecBlock.classList.add('disabled');
         metarDecBlock.classList.add('disabled');
     }
 }
 
 function metarDecr(metar) {
-    for (prop in metar) {
-        const metarInfo = metarDecBlock.querySelector(`.${prop} .metar-info`);
-        let textValue;
+    let outputString = '';
+    _propSequense.forEach((props) => {
+        props.neededProps.forEach((prop) => {
+            let target, localString = '';
+            const {prefix, postfix} = prop;
+            // console.log(prop);
 
-        if (metarInfo) {
-            switch (prop) {
-                case 'observed':
-                    const time = new Date(metar[prop]);
-                    translate(time.toUTCString().toLowerCase(), metarInfo);
-                    break;
-                case 'wind':
-                    const {degrees, speed_mps} = metar[prop];
-                    textValue = `направление: ${degrees}°, скорость: ${speed_mps} м/с`;
-                    break;
-                case 'temperature':
-                    const {celsius: temp} = metar[prop];
-                    const {celsius: dew} = metar.dewpoint;
-                    textValue = `${temp}°C / ${dew}°C`;
-                    break;
-                case 'barometer':
-                    const {mb} = metar[prop];
-                    textValue = `${mb} ГПа`;
-                    break;
-                case 'visibility':
-                    const {meters_float: meters} = metar[prop];
-                    textValue = `${meters} м`;
-                    break;
-                case 'clouds':
-                    const {code, text} = metar[prop][0];
-                    translate(text, metarInfo);
-                    break;
-                case 'humidity':
-                    const {percent} = metar[prop];
-                    textValue = `${percent}%`;
-                    break;
-                default:
-                    console.log(`${prop} не обработано`);
+            if (props.metarProp === 'clouds' || props.metarProp === 'conditions') {
+                let lastIndex = metar[props.metarProp].length - 1;
+                target = metar[props.metarProp][lastIndex][prop.name];
+            } else {
+                target = metar[props.metarProp][prop.name];
             }
-            metarInfo.textContent = textValue;
-        }
 
-    }
-}
-
-function translate(text, field) {
-    fetch(`${_apiUrls.yandex}?key=${_apiKeys.yandex}&text=${text}&lang=ru&format=plain&options=1`)
-        .then(res => res.json())
-        .then(res => {
-            field.textContent = res.text[0].toLowerCase();
-        })
-        .catch(e => {
-            console.log(e);
-            field.textContent = 'Информация временно недоступна'
+            if (target) {
+                switch (prop.name) {
+                    case 'base_feet_agl':
+                        break;
+                    case 'code':
+                        if (target === 'CAVOK') {
+                            localString = `${_dictionary.clouds[target]}. `;
+                        } else {
+                            localString = `облачность ${_dictionary.clouds[target]} на высоте ${metar[props.metarProp][0].base_feet_agl}'. `;
+                        }
+                        break;
+                    case 'meters_float':
+                        target = target / 1000;
+                        if (target === 10) target = 'более 10';
+                        localString = metFormat(prefix, target, postfix);
+                        break;
+                    default:
+                        localString += metFormat(prefix, target, postfix);
+                }
+            }
+            outputString += localString;
         });
+    });
+
+    function metFormat(x, y, z) {
+        return `${x ? x + ' ' : ''}${y}${z ? z + '. ' : '. '}`
+    }
+
+    return outputString.toUpperCase();
 }
